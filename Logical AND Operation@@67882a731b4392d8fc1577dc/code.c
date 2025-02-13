00d2982e7bb16a12a8f03c6480ff024174f12ace
Manan1903 <goyalmanan786@gmail.com>
2025-02-13 10:39:37 +0530

    Solution to Logical AND Operation

---
+++ Logical AND Operation@@67882a731b4392d8fc1577dc/code.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    double num1, num2;
+    
+    scanf("%lf", &num1);
+    
+
+    scanf("%lf", &num2);
+    
+    // Checking if num1 is equal to num2
+    if (num1 && num2) {
+        printf("True");
+    } else {
+        printf("False");
+    }
+    
+    return 0;
+}
